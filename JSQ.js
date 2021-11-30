2f0f009a2fa43c844e939075b3a93e5e9c13cbbc
Earnest Johnson <ELJohnson1@student.fullsail.edu>
2021-11-29 21:39:51 -0500

    Added Quiz javascript

--- JSQ.js
+++ JSQ.js
@@ -1,89 +1,140 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
-const WINNING_COMBINATIONS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+// Questions will be asked
+const Questions = [{
+	id: 0,
+	q: "What is capital of India?",
+	a: [{ text: "gandhinagar", isCorrect: false },
+	{ text: "Surat", isCorrect: false },
+	{ text: "Delhi", isCorrect: true },
+	{ text: "mumbai", isCorrect: false }
+	]
+
+},
+{
+	id: 1,
+	q: "What is the capital of Thailand?",
+	a: [{ text: "Lampang", isCorrect: false, isSelected: false },
+	{ text: "phuket", isCorrect: false },
+	{ text: "Ayutthaya", isCorrect: false },
+	{ text: "Bangkok", isCorrect: true }
+	]
+
+},
+{
+	id: 2,
+	q: "What is the capital of Gujarat",
+	a: [{ text: "surat", isCorrect: false },
+	{ text: "vadodara", isCorrect: false },
+	{ text: "gandhinagar", isCorrect: true },
+	{ text: "rajkot", isCorrect: false }
+	]
+
+}
+
 ]
-const cellElements = document.querySelectorAll('[data-cell]')
-const board = document.getElementById('board')
-const winningMessageElement = document.getElementById('winningMessage')
-const restartButton = document.getElementById('restartButton')
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
-let circleTurn
 
-startGame()
+// Set start
+var start = true;
 
-restartButton.addEventListener('click', startGame)
+// Iterate
+function iterate(id) {
 
-function startGame() {
-    circleTurn = false
-    cellElements.forEach(cell => {
-        cell.classList.remove(X_CLASS)
-        cell.classList.remove(CIRCLE_CLASS)
-        cell.removeEventListener('click', handleClick)
-        cell.addEventListener('click', handleClick, { once: true })
-    })
-    setBoardHoverClass()
-    winningMessageElement.classList.remove('show')
+	// Getting the result display section
+	var result = document.getElementsByClassName("result");
+	result[0].innerText = "";
+
+	// Getting the question
+	const question = document.getElementById("question");
+
+
+	// Setting the question text
+	question.innerText = Questions[id].q;
+
+	// Getting the options
+	const op1 = document.getElementById('op1');
+	const op2 = document.getElementById('op2');
+	const op3 = document.getElementById('op3');
+	const op4 = document.getElementById('op4');
+
+
+	// Providing option text
+	op1.innerText = Questions[id].a[0].text;
+	op2.innerText = Questions[id].a[1].text;
+	op3.innerText = Questions[id].a[2].text;
+	op4.innerText = Questions[id].a[3].text;
+
+	// Providing the true or false value to the options
+	op1.value = Questions[id].a[0].isCorrect;
+	op2.value = Questions[id].a[1].isCorrect;
+	op3.value = Questions[id].a[2].isCorrect;
+	op4.value = Questions[id].a[3].isCorrect;
+
+	var selected = "";
+
+	// Show selection for op1
+	op1.addEventListener("click", () => {
+		op1.style.backgroundColor = "lightgoldenrodyellow";
+		op2.style.backgroundColor = "lightskyblue";
+		op3.style.backgroundColor = "lightskyblue";
+		op4.style.backgroundColor = "lightskyblue";
+		selected = op1.value;
+	})
+
+	// Show selection for op2
+	op2.addEventListener("click", () => {
+		op1.style.backgroundColor = "lightskyblue";
+		op2.style.backgroundColor = "lightgoldenrodyellow";
+		op3.style.backgroundColor = "lightskyblue";
+		op4.style.backgroundColor = "lightskyblue";
+		selected = op2.value;
+	})
+
+	// Show selection for op3
+	op3.addEventListener("click", () => {
+		op1.style.backgroundColor = "lightskyblue";
+		op2.style.backgroundColor = "lightskyblue";
+		op3.style.backgroundColor = "lightgoldenrodyellow";
+		op4.style.backgroundColor = "lightskyblue";
+		selected = op3.value;
+	})
+
+	// Show selection for op4
+	op4.addEventListener("click", () => {
+		op1.style.backgroundColor = "lightskyblue";
+		op2.style.backgroundColor = "lightskyblue";
+		op3.style.backgroundColor = "lightskyblue";
+		op4.style.backgroundColor = "lightgoldenrodyellow";
+		selected = op4.value;
+	})
+
+	// Grabbing the evaluate button
+	const evaluate = document.getElementsByClassName("evaluate");
+
+	// Evaluate method
+	evaluate[0].addEventListener("click", () => {
+		if (selected == "true") {
+			result[0].innerHTML = "True";
+			result[0].style.color = "green";
+		} else {
+			result[0].innerHTML = "False";
+			result[0].style.color = "red";
+		}
+	})
 }
 
-function handleClick(e) {
-    const cell = e.target
-    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
-    placeMark(cell, currentClass)
-    if (checkWin(currentClass)) {
-        endGame(false)
-    } else if (isDraw()) {
-        endGame(true)
-    } else {
-        swapTurns()
-        setBoardHoverClass()
-    }
+if (start) {
+	iterate("0");
 }
 
-function endGame(draw) {
-    if (draw) {
-        winningMessageTextElement.innerText = 'Draw!'
-    } else {
-        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
-    }
-    winningMessageElement.classList.add('show')
-}
+// Next button and method
+const next = document.getElementsByClassName('next')[0];
+var id = 0;
 
-function isDraw() {
-    return [...cellElements].every(cell => {
-        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
-    })
-}
+next.addEventListener("click", () => {
+	start = false;
+	if (id < 2) {
+		id++;
+		iterate(id);
+		console.log(id);
+	}
 
-function placeMark(cell, currentClass) {
-    cell.classList.add(currentClass)
-}
-
-function swapTurns() {
-    circleTurn = !circleTurn
-}
-
-function setBoardHoverClass() {
-    board.classList.remove(X_CLASS)
-    board.classList.remove(CIRCLE_CLASS)
-    if (circleTurn) {
-        board.classList.add(CIRCLE_CLASS)
-    } else {
-        board.classList.add(X_CLASS)
-    }
-}
-
-function checkWin(currentClass) {
-    return WINNING_COMBINATIONS.some(combination => {
-        return combination.every(index => {
-            return cellElements[index].classList.contains(currentClass)
-        })
-    })
-}
+})
